c752d47815aded751cf010bd1b685bd801b278ec
i7i8i9com <752399672@qq.com>
2022-11-27 14:49:31 +0800

    add 跨域

--- src/main/java/com/i7i8i9/musicbird1/controller/DefaultController.java
+++ src/main/java/com/i7i8i9/musicbird1/controller/DefaultController.java
@@ -1,9 +1,11 @@
 package com.i7i8i9.musicbird1.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin
 public class DefaultController {
     @GetMapping
     public String sayHello(){
